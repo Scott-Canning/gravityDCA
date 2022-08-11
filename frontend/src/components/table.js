db44642f295fc639483e2a5d9bdd0e09bfd678bd
Scott-Canning <34758484+Scott-Canning@users.noreply.github.com>
2022-08-11 14:11:04 -0400

    updated portfolio view table with additional functionality and refined UI

--- frontend/src/components/table.js
+++ frontend/src/components/table.js
@@ -2,38 +2,27 @@ import React from 'react';
 import { useTable } from "react-table";
 import './styles/table.css';
 
-const Table = ({ columns, data }) => {
-
+const Table = ({ columns, data, getTrProps = props => props}) => {
     const {
         getTableProps,      // table props from react-table
         getTableBodyProps,  // table body props from react-table
-        headerGroups,       // headerGroups, if your table has groupings
         rows,               // rows for the table based on the data passed
         prepareRow          // Prepare the row (this function needs to be called for each row before getting the row props)
     } = useTable({
         columns,
-        data
+        data,
     });
 
     return (
         <div className='table-container'>
             <table {...getTableProps()} className='table'>
-                <thead className='table-head'>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map(column => (
-                        <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-                    ))}
-                    </tr>
-                ))}
-                </thead>
                 <tbody {...getTableBodyProps()} className='table-body'>
                 {rows.map((row, i) => {
                     prepareRow(row);
                     return (
-                    <tr {...row.getRowProps()}>
+                    <tr key={i} {...row.getRowProps()}  {...getTrProps(row)}>
                         {row.cells.map(cell => {
-                        return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                            return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
                         })}
                     </tr>
                     );
--- frontend/src/views/portfolio.js
+++ frontend/src/views/portfolio.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import './styles/portfolio.css';
 import Header from '../components/header';
 import Menu from '../components/menu';
@@ -7,62 +7,86 @@ import { fundingAssetMap } from '../utilities/fundingAssetMap';
 import { pairIdMap } from '../utilities/pairIdMap';
 
 const Portfolio = () => {
+    const[selectedRow, setSelectedRow] = useState("");
 
     const columns = useMemo(
         () => [
-                {
-                    Header: "Pair",
-                    accessor: "pair_id",
-                    Cell: ({ cell: { value } }) => {return (
-                        <div>
+            {
+                Header: "Pair",
+                accessor: "pair_id",
+                Cell: ({ cell: { value } }) => { return (
+                    <div className='cell-style-75'>
+                        <div className='pair-wrapper'>
                             <img src={fundingAssetMap[pairIdMap[value].from]} className='img__pair-from-token'/>
+                            <p><i class="arrow right"></i></p>
                             <img src={fundingAssetMap[pairIdMap[value].to]} className='img__pair-to-token'/>
                         </div>
-                    )}
-                },
-                {
-                    Header: "Status",
-                    accessor: "status",
-                    Cell: ({ cell: { value } }) => {return (value === 'Live' ? 
-                        (<div className='live-badge'>Live</div>) :
-                        (<div className='ended-badge'>Ended</div>)
-                    )}
-                },
-                {
-                    Header: "Balance",
-                    accessor: "balance",
-                    Cell: ({ cell: { row, value } }) => {return (
-                    <div className='balance-container'>
-                        <div className='balance-value'>{ value }</div>
-                        <div className='divider'></div>
-                        <div className='balance-target-asset-image-container'>
-                            <img src={fundingAssetMap[pairIdMap[row.original.pair_id].to]} className='img__balance-target-asset'/>
+                    </div>
+                )},
+            },
+            {
+                Header: "Status",
+                accessor: "status",
+                Cell: ({ cell: { value } }) => { return (value === 'Live' ? 
+                    (<div className='cell-style-75'>
+                        <div className='live-badge'>Live</div>
+                    </div>) :
+                    (<div className='cell-style-75'>
+                        <div className='ended-badge'>Ended</div>
+                    </div>)
+                )}
+            },
+            {
+                Header: "Balance",
+                accessor: "balance",
+                Cell: ({ cell: { row, value } }) => { return (
+                    <div className='cell-style-120'>
+                        <div className='balance-container'>
+                            <div className='balance-value'>{ parseFloat(value).toFixed(2) }</div>
+                            <div className='divider'></div>
+                            <div className='balance-target-asset-image-container'>
+                                <img src={fundingAssetMap[pairIdMap[row.original.pair_id].to]} className='img__balance-target-asset'/>
+                            </div>
                         </div>
                     </div>)},
-                },
-                {
-                    Header: "Next Purchase",
-                    accessor: "next_purchase",
-                    Cell: ({ cell: { value } }) => value || "NA"
-                },
-                {
-                    Header: "Remaining",
-                    accessor: "remaining",
-                    Cell: ({ cell: { value } }) => value || "0"
-                },
-                {
-                    Header: "",
-                    accessor: "top_up",
-                    Cell: ({ cell: { row, value } }) => {return (row.original.status === 'Live' ? 
-                        (<div><button className='button-top-up__portfolio'>Top Up</button></div>) :
-                        (<div></div>)
-                    )},
-                },
-                {
-                    Header: "",
-                    accessor: "withdraw",
-                    Cell: () => {return(<div><button className='button-withdraw__portfolio'>Withdraw</button></div>)},
-                },
+            },
+            {
+                Header: "Next Purchase",
+                accessor: "next_purchase",
+                Cell: ({ cell: { value } }) => { return (value ? 
+                    (<div className='cell-style-105'>{value}</div>) :
+                    (<div className='cell-style-105'>NA</div>)
+                )},
+            },
+            {
+                Header: "Remaining",
+                accessor: "remaining",
+                Cell: ({ cell: { value } }) => { return (value ? 
+                    (<div className='cell-style-105'>{value}</div>) :
+                    (<div className='cell-style-105'>0</div>)
+                )},
+            },
+            {
+                Header: "",
+                accessor: "top_up",
+                Cell: ({ cell: { row } }) => { return (row.original.status === 'Live' ? 
+                    (<div className='cell-style'>
+                        <div><button className='button-top-up__portfolio'>Top Up</button></div>
+                    </div>) :
+                    (<div className='cell-style'>
+                        <div></div>
+                    </div>)
+                )},
+            },
+            {
+                Header: "",
+                accessor: "withdraw",
+                Cell: ({ cell: { row } }) => {return(
+                    <div className='cell-style'>
+                        <button className='button-withdraw__portfolio'>Withdraw</button>
+                    </div>)},
+            },
+            // top_up & withdraw -> onClick, pass row.original.pair_id prop
         ]
     );
     
@@ -102,9 +126,16 @@ const Portfolio = () => {
             "next_purchase": '9/1/22',
             "remaining": 5
         },
+        {
+            "pair_id": 6,
+            "status": "Ended",
+            "balance": 2.21,
+            "next_purchase": '',
+            "remaining": 0
+        },
         // {
-        //     "pair_id": 6,
-        //     "status": "Ended",
+        //     "pair_id": 2,
+        //     "status": "Live",
         //     "balance": 2.21,
         //     "next_purchase": '',
         //     "remaining": 0
@@ -121,11 +152,29 @@ const Portfolio = () => {
                     <Menu/>
                 </div>
                 <div className='portfolio-container'>
-                    {/* <div className='title-container__portfolio'>
-                        <p className='title__portfolio'>Your Strategies</p>
-                    </div> */}
+                    <div className='table-header-container__portfolio'>
+                        <div className='table-header__portfolio'>
+                            <div style={{width: '9px'}}/>
+                            <p className='table-header-style-75'>Pair</p>
+                            <p className='table-header-style-75'>Status</p>
+                            <p className='table-header-style-120'>Balance</p>
+                            <p className='table-header-style-105'>Next Buy</p>
+                            <p className='table-header-style-105'>Remaining</p>
+                        </div>
+                    </div>
                     <div className='strategies-container'>
-                        <Table columns={columns} data={data} />
+                        <Table columns={columns} data={data} 
+                            getTrProps={(row) => ({
+                            style: { cursor: "auto" },
+                                onClick: () => {
+                                    setSelectedRow(row.id);
+                                },
+                                style: {
+                                    background: row.id === selectedRow ? 'rgb(141, 213, 128)' : '',
+                                    color: row.id === selectedRow ? 'black' : ''
+                                }
+                            })}
+                        />
                     </div>
                     <div className='deployment-schedule-container__portfolio'>
 
